691b41b35ff608bc365ff3fff4ae6ebc440e4c6c
Jonathan Vazquez <vamtha24@gmail.com>
2018-07-09 21:12:23 -0500

    Add simple validation to process args

--- bin/movie-search.js
+++ bin/movie-search.js
@@ -3,35 +3,39 @@
 const https = require('https');
 const cheerio = require('cheerio');
 
-const [ movieName ] = process.argv.slice(2);
+const [movieName] = process.argv.slice(2);
+
+if (!movieName) {
+  console.log('Please provide Title.');
+  process.exit(1);
+}
+
 const URL = makeUrl(movieName);
 
 imdbSearch(URL, (err, movies) => {
-  if(err) throw err;
-  if(movies.length > 0) {
+  if (err) throw err;
+  if (movies.length > 0) {
     movies.forEach(movie => console.log(movie));
     process.exit(1);
   }
   console.log(`No results found for "${movieName}"`);
 });
 
-
-
 function makeUrl(query) {
   return `https://www.imdb.com/find?ref_=nv_sr_fn&q=${query}&s=all`;
-};
+}
 
 function imdbSearch(url, cb) {
-  https.get(url, (response) => {
+  https.get(url, response => {
     let html;
     response.setEncoding('utf8');
-    response.on('data', chunk => (html+= chunk));
+    response.on('data', chunk => (html += chunk));
     response.on('end', () => {
       const moviesData = pageParser(html);
       cb(null, moviesData);
     });
   });
-};
+}
 
 function pageParser(page) {
   const $ = cheerio.load(page);
@@ -39,6 +43,10 @@ function pageParser(page) {
   const $movieList = $resultsSection.find('.findResult .result_text').toArray();
 
   return $movieList.map(elem =>
-    $(elem).contents().not($(elem).children('small')).text().trim()
+    $(elem)
+      .contents()
+      .not($(elem).children('small'))
+      .text()
+      .trim()
   );
-};
+}
